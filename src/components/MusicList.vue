844a332114bb80b06e6b06f0528cd6feec61bd06
Zeng Tao <qingchun1995@gmail.com>
2021-03-02 13:39:04 +0800

    change: create the MusicList component

---
+++ src/components/MusicList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="music-list">
+    <!-- 封面图 -->
+    <div class="top"></div>
+    <!-- 歌曲列表 -->
+    <div class="song-list"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MusicList'
+}
+</script>
+
+<style lang="scss" scoped></style>
